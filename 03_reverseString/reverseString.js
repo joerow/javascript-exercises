afd6986e8f7866866ac3f6707d496b12a9323c95
joerow <joerow@gmail.com>
2022-04-03 15:39:48 +0800

    add 03 solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (textToSplit) {
 
+    let textArray = Array.from(textToSplit);
+    let finalChar = (textArray.length - 1);
+    let reversedText = "";
+    for (let i=finalChar; i>=0; i--) {
+        reversedText += textArray[i];
+    }
+    return reversedText;
+    //return textArray[finalChar-1];
 };
 
 // Do not edit below this line
