bbb385f4a66dd748ced19418e5cfafa800af96ef
joerow <joerow@gmail.com>
2022-04-03 15:05:05 +0800

    add 01 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (text, numOf) {
+    let finalString = "";
+    if (numOf > 0) {
+        for (let i = 0; i < numOf; i++) {
+            finalString += text;
+        }
+        return finalString;
+    } else if (numOf === 0) {
+        return finalString;
+    }
+    else {
+        finalString = "ERROR";
+        return finalString;
+    }
 };
 
 // Do not edit below this line
