442c41ef6bc965421756a26704205f5a2a170c63
joerow <joerow@gmail.com>
2022-04-04 10:29:21 +0800

    add 07 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let celcius = ((farenheit - 32) * (5/9));
+  return parseFloat(celcius.toFixed(1)); 
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let farenheit = ((celcius * (9/5)) + 32);
+  return parseFloat(farenheit.toFixed(1));
 };
 
 // Do not edit below this line
