e531f9db3af2ece8491bfcd4de3e29f11b82d98b
joerow <joerow@gmail.com>
2022-04-11 10:35:25 +0800

    prettier formatting

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,17 +1,16 @@
 const repeatString = function (text, numOf) {
-    let finalString = "";
-    if (numOf > 0) {
-        for (let i = 0; i < numOf; i++) {
-            finalString += text;
-        }
-        return finalString;
-    } else if (numOf === 0) {
-        return finalString;
+  let finalString = "";
+  if (numOf > 0) {
+    for (let i = 0; i < numOf; i++) {
+      finalString += text;
     }
-    else {
-        finalString = "ERROR";
-        return finalString;
-    }
+    return finalString;
+  } else if (numOf === 0) {
+    return finalString;
+  } else {
+    finalString = "ERROR";
+    return finalString;
+  }
 };
 
 // Do not edit below this line
