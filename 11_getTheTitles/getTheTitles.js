99b4f3329b0a7e3d5e86c0ed96924095a4b1ee3b
joerow <joerow@gmail.com>
2022-04-11 10:42:54 +0800

    add 11 solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,10 @@
-const getTheTitles = function (mainArray) {};
+const getTheTitles = function (mainArray) {
+  let myArray = [];
+  mainArray.forEach((book) => {
+    myArray.push(book["title"]);
+  });
+  return myArray;
+};
 
 // Do not edit below this line
 module.exports = getTheTitles;
