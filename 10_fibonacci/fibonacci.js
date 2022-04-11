59bfb6be462b671625b970d265703a584736cd4b
joerow <joerow@gmail.com>
2022-04-11 10:17:00 +0800

    add 10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (val) {
+  if (val < 0) {
+    return "OOPS";
+  }
+  if (val == 1) {
+    return 1;
+  }
+  let n1 = 0,
+    n2 = 1,
+    nextTerm;
+  for (let index = 1; index < val; index++) {
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
+  console.log(nextTerm);
+  return nextTerm;
 };
 
 // Do not edit below this line
